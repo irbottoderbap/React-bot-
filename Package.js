197c2b053a9a285d32a40518afde45f5a147da56
irbottoderbap <israfilcoc400@gmail.com>
2025-11-23 17:09:16 +0600

    Update and rename README.md to Package.js

---
+++ Package.js
@@ -0,0 +1,19 @@
+{
+  "name": "specific-user-bot",
+  "version": "1.0.0",
+  "description": "Bot that reacts to specific users",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
